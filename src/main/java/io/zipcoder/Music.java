c5134961b798ec6465943949b72321a642c484d4
taylor <taylormyers01@comcast.net>
2023-07-11 11:07:28 -0400

    Updated music functions and verified all tests passed

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -3,12 +3,45 @@ package io.zipcoder;
 public class Music {
 
     private String[] playList;
+    private int currentIndex;
 
     public Music(String[] playList){
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int nextSongNum = 0;
+        int prevSongNum = 0;
+        this.currentIndex = startIndex;
+        while(!playList[currentIndex].equals(selection)){
+            nextSong();
+            nextSongNum++;
+        }
+        this.currentIndex = startIndex;
+        while(!playList[currentIndex].equals(selection)){
+            previousSong();
+            prevSongNum++;
+        }
+        if(prevSongNum<nextSongNum){
+            return prevSongNum;
+        }
+        return nextSongNum;
+    }
+
+    private void nextSong(){
+        if(this.currentIndex >= playList.length){
+            this.currentIndex = 0;
+        }
+        else{
+            this.currentIndex++;
+        }
+    }
+    public void previousSong(){
+        if(this.currentIndex == 0){
+            this.currentIndex = playList.length-1;
+        }
+        else{
+            this.currentIndex--;
+        }
     }
 }
